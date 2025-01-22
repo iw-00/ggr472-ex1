17bcf835b4e8720d530efb4ac126d442edadd69a
iw-00 <irisy.wu@mail.utoronto.ca>
2025-01-22 15:59:00 -0500

    changing button

---
+++ README.md
@@ -0,0 +1,2 @@
+# ggr472-ex1
+ Demo website from GGR472 Week 2 example.
---
+++ script.js
@@ -0,0 +1,7 @@
+// Initialize button variable.
+const btn = document.getElementById("my-button");
+
+// Add action when button is clicked.
+btn.addEventListener("click", () => {
+    console.log("You clicked me!");
+});
